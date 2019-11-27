c704e6ceb1f814c841067def308c58f1e3e14be0
서진원 <seojin-won@seojin-won-ui-MacBook-Air.local>
2019-11-27 20:42:09 +0900

    add signup info

--- Users/views.py
+++ Users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from users import models
 
 
 def signup(request):
@@ -9,11 +10,18 @@ def signup(request):
       username = request.POST.get("username"," ")
       password = request.POST.get("password1"," ")
       email = request.POST.get("email", " ")
-      
+
+      name = request.POST.get("name", " ")
+      gender = request.POST.get("gender", " ")
+      age = request.POST.get("age", " ")
+      # ticket = request.POST.get("ticket", " ")
+
       user = User.objects.create_user(username,email,password)
       user.save()
       #auth.login(user)
-
+      
+      Data(name=name, uid=username, gender=gender, age=age)
+      
       return redirect('/index/')
     return redirect('signup')
   return render(request, 'signup.html')
@@ -36,6 +44,5 @@ def login(request):
 def logout(request):
   auth.logout(request)
   return redirect('/index')
-
 
 # Create your views here.
--- festivals/views.py
+++ festivals/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from festivals.models import festival
+from django.contrib import messages
 
 # Create your views here.
-def Register(request):
+def RegisterFestival(request):
   if request.method =="POST" :
     festivalName = request.POST.get("fes_name", " ")
     festivalDate = request.POST.get("fes_date", " ")
@@ -13,5 +14,14 @@ def Register(request):
     is_festival_in = festival.objects.filter(name=festivalName)
     if is_festival_in == 0: 
       festival(name=festivalName, date=festivalDate, place=festivalPlace, price=festivalPrice, pic=festivalPicture)
+    else:
+      messages.info("등록하려 하는 축제 정보가 이미 DB안에 존재합니다!")
+      redirect('/festival/')
 
+    messages.info("등록되었습니다")
+    redirect('/festival')
+  else:
+    return render(request, 'festival.html')
+  
+  
   
