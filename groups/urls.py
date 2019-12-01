4ea22da97e51eaf275b7b1ebbfecfdf3b714b60a
Ju-Yeon Park <pjuyeon25@gmail.com>
2019-12-02 02:09:32 +0900

    confirm ticket

--- groups/urls.py
+++ groups/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from groups import views
 
 urlpatterns = [
-  path('', views.group, name='group'),
-  path('register/', views.register, name='registerGroup'),
-  path('<name>/', views.each)
-]
+   path('', views.group, name='group'),
+   path('register/', views.register, name='registerGroup'),
+   path('<int:id>/', views.each)
+ ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from users.models import Data
 # from festivals.models import Festival
-
+from groups.models import Groups
         
 # Create your views here.
 
@@ -22,4 +22,30 @@ def mypage(request):
   return render(request, 'mypage.html', context)
 
 def confirmTicket(request):
-  return render(request, 'confirmTicket.html')
+  if request.method=="POST":
+    accept = request.POST.get("accept", None)
+    denial = request.POST.get("denial", None)
+
+    if accept is not None and denial is None:
+      group = Groups.objects.get(name=accept)
+      group.is_authenticated = 1
+      group.save()
+    else:
+      group = Groups.objects.get(name=denial)
+      group.is_authenticated = -1
+      group.save()
+
+    return redirect('/manager/confirm_ticket')
+  else:
+    #n = Groups.objects.filter(is_authenticated=0).count()
+    #if(n==1):
+    groups = Groups.objects.filter(is_authenticated=0)
+    #else:
+    # GroupList = Groups.objects.filter(is_authenticated=0)
+
+    context = {'groups': groups}
+    return render(request, 'confirmTicket.html', context)
+
+  #groups = Groups.objects.filter(is_authenticated=0)
+  #context = {'groups': groups}
+  #return render(request, 'confirmTicket.html', context)
