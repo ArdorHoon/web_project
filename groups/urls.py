cb794b70ce4a7e9eb6b18a1c86471296221d3f67
hunmoyang <didgnsah7320@gmail.com>
2019-12-02 21:22:47 +0900

    correct

--- groups/urls.py
+++ groups/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
    path('', views.group, name='group'),
    path('register/', views.register, name='registerGroup'),
    path('<int:id>/apply/', views.apply ),
-   path('<int:id>/', views.each)
+   path('<name>/', views.each)
  ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
