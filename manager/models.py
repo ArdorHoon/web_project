df9a1df35e7bf055a159e2d4676a6b5d238d09f2
hyelynn <jmnu1203@hanmail.net>
2019-12-03 19:22:22 +0900

    groups

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Festival(models.Model):
+  name = models.CharField(max_length=30, default="")
+  date = models.DateField(default=datetime.date.today)
+  place = models.CharField(max_length=30, default="")
+  price = models.IntegerField(default=0, blank=True, null=True)
+  poster = models.ImageField(upload_to="festival", null=True, blank=True)
