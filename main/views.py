bfab862d5d545ff21cce379bc156957e81edf6fb
Ju-Yeon Park <pjuyeon25@gmail.com>
2019-12-03 17:24:33 +0900

    그룹 탈퇴 in mypage

--- main/views.py
+++ main/views.py
@@ -58,4 +58,43 @@ def confirmTicket(request):
   #context = {'groups': groups}
   #return render(request, 'confirmTicket.html', context)
 
+def confirmGroup(request,name):
+  if request.method=="POST":
+    accept = request.POST.get("accept", None)
+    denial = request.POST.get("denial", None)
 
+    group = Groups.objects.get(name=name)
+
+    if accept is not None and denial is None:
+      groupuser = Groupusers.objects.get(group_name=group.name, user_id=accept)
+      print(groupuser.group_name, groupuser.user_id)
+      groupuser.status = 1
+      groupuser.save()
+    else:
+      groupuser = Groupusers.objects.get(group_name=group.name, user_id=denial)
+      groupuser.status = -1
+      groupuser.save()
+
+    groupusers = Groupusers.objects.filter(group_name = group.name)
+
+    context = {'group':group, 'groupusers': groupusers}
+    return render(request, 'eachGroup.html', context)
+
+  else:
+    group = Groups.objects.get(name=name)
+    groupusers = Groupusers.objects.filter(group_name = group.name)
+    context = {'group':group, 'groupusers': groupusers}
+    return render(request, 'eachGroup.html', context)
+
+
+def getout(request,name):
+    groupuser = Groupusers.objects.get(group_name=name, user_id=request.user.username)
+    groupuser.delete()
+
+    datas = Data.objects.get(uid=request.user.username) #단일 행 가져오기
+    Groups = Groupusers.objects.filter(user_id = request.user.username)
+
+    context = {'datas': datas , 'Groups' : Groups }
+
+    return render(request, 'mypage.html', context)
+   
