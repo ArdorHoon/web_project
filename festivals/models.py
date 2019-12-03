43b8b0f38b11543e6207fe894c15c2577c8fa16e
서진원 <seojin-won@seojin-won-ui-MacBook-Air.local>
2019-12-03 18:25:23 +0900

    ddwer

--- festivals/models.py
+++ festivals/models.py
@@ -4,13 +4,7 @@ import datetime
 # Create your models here.
 class Festival(models.Model):
   name = models.CharField(max_length=30, default="")
-  # time = models.CharField(max_length=50, default="")
   date = models.DateField(default=datetime.date.today)
   place = models.CharField(max_length=30, default="")
   price = models.IntegerField(default=0, blank=True, null=True)
-  # lineup = models.TextField(default="")
-<<<<<<< HEAD
   poster = models.ImageField(upload_to="image", null=True, blank=True)
-=======
-  pic = models.ImageField(upload_to="festival",null=True, blank=True)
->>>>>>> 97e5a0a3ba84d372c57f93b3f59e0536bc8755ed
