9515e393f85fe325c9cb6225689925e2c394ae36
Ju-Yeon Park <pjuyeon25@gmail.com>
2019-12-10 01:42:30 +0900

    그룹 2개 등록 및 티켓사진 업로드

--- groupusers/models.py
+++ groupusers/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Groupusers(models.Model):
   group_name = models.CharField(max_length=50, default="")
   user_id = models.CharField(max_length=10, default="")
-  status = models.IntegerField(default=0, null=True, blank=True) # 2는 리더, 1는 user, 0은 대기, -1는 거절
+  status = models.IntegerField(default=0, null=True, blank=True)
+  # 2는 리더, 1는 user, 0은 대기, -1는 거절
 
