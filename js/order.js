82ee6b225e7e72e67ab78350dacc74435496313a
hunmoyang <didgnsah7320@gmail.com>
2020-08-13 18:00:32 +0900

    add order

--- js/order.js
+++ js/order.js
@@ -1,9 +1,31 @@
+const state = {
+
+    "pay" : "결제대기",
+    "failed" : "결제실패" ,
+    "paid" : "결제완료",
+    "making" : "박스 제작중" ,
+    "req_can" : "취소 요청",
+    "can_comp" : "취소 완료",
+    "req_ref" : "환불 요청",
+    "ref_comp" : "환불 완료",
+    "delivery" : "배송중",
+    "dev_comp" : "배송 완료"
+
+};
+
+const checkPay = {
+
+    "card" : "카드",
+    "bank" : "실시간 계좌이체",
+    "vbank" : "가상계좌"
+};
 
 function orderShowBtn(data){
 
     sessionStorage.setItem("order_id",data);
     location.href ="/order/orderSpec.html";
 }
+
 
 function getOrderList(){
 
@@ -15,50 +37,49 @@ function getOrderList(){
     });
 }
 
-function checkState(data){
+function init(keyword){
 
-    if(data === "pay")
-        return "결제 대기";
-}
-
-function checkPay(data){
-
-    if(data==="card")
-         return "카드";
-    else{
-        return "무통장 입금";
-    }
-}
-
-
-function init(){
-
-
+    
     getOrderList().then(function(data){
 
-        console.log(data);
+        let date ="";
 
-             
         $.each(data, function(index, item){
+            
+        
+            if (item.order_state === keyword || keyword === 'all') {
 
-            $(".all-order-list").append(
-                `
-                <tr>
-                <td>${item.order_id}</td>
-                <td>${checkState(item.order_state)}</td>
-                <td><strong>${item.cart_name}</strong><br>${item.cart_subname}</td>
-                <td>${item.order_totalprice}원</td>
-                <td>${checkPay(item.billing_type)}</td>
-                <td>${item.user_name}</td>
-                <td>${item.order_to_name}</td>
-                <td><a href="#" onclick="orderShowBtn(${item.order_id})">주문상세보기</a></td>
-                </tr>`
-            );
+                    const dtype = (item.order_date).split('T');
+                    const rdate = dtype[0];
 
+                    if(date !== rdate){
+
+                        date= rdate;
+                        const sdate = date.split("-");
+                        $(".all-order-list").append(
+                             `<tr class="table-sm" style =" background-color: #FAFAFC;">
+                              <td colspan = "8" style="color : #590098; text-align: center;">${sdate[0]}년 ${sdate[1]}월 ${sdate[2]}일</td>
+                             </tr>`
+                          );
+                    }
+                
+                    $(".all-order-list").append(
+                    `
+                    <tr>
+                    <td>${item.order_id}</td>
+                    <td>${state[item.order_state]}</td>
+                    <td><strong>${item.cart_name}</strong><br>${item.cart_subname}</td>
+                    <td>${item.order_totalprice}원</td>
+                    <td>${checkPay[item.billing_type]}</td>
+                    <td>${item.user_name}</td>
+                    <td>${item.order_to_name}</td>
+                    <td><a href="#" onclick="orderShowBtn('${item.order_id}')">주문상세보기</a></td>
+                    </tr>`
+                    );
+            }
         });
     });
 }
 
 
 
-init();
--- js/orderSpec.js
+++ js/orderSpec.js
@@ -1,3 +1,66 @@
+const orderId = sessionStorage.getItem("order_id"); 
+
+const state = {
+
+    "pay" : "결제대기",
+    "failed" : "결제실패" ,
+    "paid" : "결제완료",
+    "making" : "박스 제작중" ,
+    "req_can" : "취소 요청",
+    "can_comp" : "취소 완료",
+    "req_ref" : "환불 요청",
+    "ref_comp" : "환불 완료",
+    "delivery" : "배송중",
+    "dev_comp" : "배송 완료"
+
+};
+
+const checkPay = {
+
+    "card" : "카드",
+    "bank" : "실시간 계좌이체",
+    "vbank" : "가상계좌",
+    undefined : "undefined!"
+};
+
+function AddComma(data_value) {
+ 
+    return Number(data_value).toLocaleString('en');
+    
+}
+
+$(".changeOrderInfo").click(function(){
+
+    const orderState = document.querySelector("#orderState"); //product_kind.options[target.selectedIndex].value
+    const state = orderState.options[orderState.selectedIndex].value;
+
+    const company = $("#delivery1").val(); //택배회사
+    const com_num = $("#delivery2").val(); //송장번호
+    const del_memo = $("#delivery3").val(); //배송 메모
+    const memo = $("#memo").val(); //메모
+    const name =  $("#username").val(); // 수령인 이름 
+    const postcode =  $("#addressNumber").val(); //우편 번호
+    const addr1 = $("#userAddress").val(); //도로명 주소
+    const addr2 = $("#userAddress2").val(); //상세 주소
+    const phone = $("#phonenumber").val(); //연락처 
+
+    
+    $.post("http://13.209.181.48:3000/order/modify/host", { _id : orderId, _state : state, _devco : company, _devno : com_num , _devmemo : del_memo,
+    _memo : memo , _name : name , _post : postcode , _addr : addr1 , _sub : addr2 , _phone : phone }, function(data){
+         
+        console.log(data);
+            if(data.result === "complete"){
+                
+               location.href ="/order/allOrder.html";
+                
+            }
+    });
+    
+        
+
+});
+
+
 function DaumPostcode() {
     new daum.Postcode({
         oncomplete: function(data) {
@@ -15,4 +78,83 @@ function DaumPostcode() {
     }).open();
 }
 
+//Primoise object 
+function getAllOrderData(){
+
+    return new Promise(function(resolve, reject){
+
+        $.post('http://13.209.181.48:3000/order/info', { _id : orderId } , function(response){
+            resolve(response);
+        });
+    });
+
+}
+
+function InputOrderData(data){
+
+    console.log(data);
+    const date = data[0].order_date.split("T");
+    const total = data[0].order_totalprice.split(",");
+    const dprice = data[0].billing_dprice.split(",");
+    let price = "";
+    let dp ="";
+  
+    total.forEach(e => {
+        price += e
+    });
+    dprice.forEach(e=>{
+        dp +=e;
+    });
+    let totalprice = String(Number(price) + Number(dp));
+
+    $(".orderUsename").text(data[0].user_name); //주문자 이름
+    $(".orderNumber").text(data[0].order_id); //주문 번호
+    $("#ordercode").val(data[0].order_id); //주문 번호
+    $("#orderDay").val(date[0]); //주문 일자 
+    $("#orderState").val(data[0].order_state).prop("selected", true); //주문 상태
+    $("#delivery1").val(data[0].order_dev_co); //택배회사
+    $("#delivery2").val(data[0].order_dev_no); //송장번호
+    $("#delivery3").val(data[0].order_dev_memo); //배송 메모
+    $("#memo").val(data[0].order_memo); //메모
+    $("#username").val(data[0].order_to_name); // 수령인 이름 
+    $("#addressNumber").val(data[0].order_to_postcode); //우편 번호
+    $("#userAddress").val(data[0].order_to_addr); //도로명 주소
+    $("#userAddress2").val(data[0].order_to_sub); //상세 주소
+    $("#phonenumber").val(data[0].order_to_phone); //연락처 
+    $("#name").val(data[0].user_name); //주문자 이름 
+    $("#phoneNum").val(data[0].user_phone); // 주문자 휴대폰번호
+    $("#staticEmail").val(data[0].order_user); //주문자 이메일
+    $("#totalPrice").val(`${data[0].order_totalprice}원`); //상품 합계
+    $("#deliveryCount").val(`${data[0].billing_dprice}원`); //배송비
+    $("#payMoney").val(`${AddComma(totalprice)}원`);
+    $("#payMethod").val(`${checkPay[data[0].billing_type]}`); //결제 방법
+
+}
+
+
+function init(){
+    
+    let cart_id;
+
+    getAllOrderData().then(function(data){
+
+        cart_id = data[0].cart_id;
+  
+        InputOrderData(data);
+
+    }).then(function() {
+        
+
+        $.post('http://13.209.181.48:3000/order/info/cart', { _id : cart_id } , function(data){
+          console.log(data);
+        });
+
+
+    });
+
+    
+}
+
+
+init();
 
